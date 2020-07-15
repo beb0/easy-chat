9edaba101f6ed515759bfc490407bbccde1e65d0
abd-diaa <abdelrahman.diaa.g@gmail.com>
2020-07-15 20:38:10 +0200

    added message emitting

--- index.js
+++ index.js
@@ -13,4 +13,8 @@ var io = socket(server)
 
 io.on('connection', function(socket){
     console.log(`connection made at ${socket.id}`);
+
+    socket.on('message', function(message){
+        io.sockets.emit('message', message)
+    })
 })
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,18 @@
 //front end connection
-
 var socket = io.connect("http://localhost:4000")
+
+var message = document.getElementById('message'),
+      usr = document.getElementById('username'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
+
+btn.addEventListener('click', function() {
+    socket.emit('message', {
+        message : message.value,
+        username : usr.value
+    })
+})
+
+socket.on('message', function(data){
+    output.innerHTML += '<p><strong>' + data.username + ':</strong> ' + data.message + '</p>'
+})
